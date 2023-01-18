c6b583eb7380c02db4e97ca748cd1f7035f17a64
goryanski <53867408+goryanski@users.noreply.github.com>
2023-01-18 21:14:24 +0200

    Authorization is done

--- src/main/java/com/softserve/itacademy/config/SecurityConfig.java
+++ src/main/java/com/softserve/itacademy/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.softserve.itacademy.Config;
+package com.softserve.itacademy.config;
 
 import com.softserve.itacademy.security.PersonDetailsService;
 import com.softserve.itacademy.security.WebAccessDeniedHandler;
@@ -17,7 +17,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final PersonDetailsService personDetailsService;
     private final WebAccessDeniedHandler webAccessDeniedHandler;
 
-
+    private static final String[] ADMIN_ENDPOINTS = {
+            "/",
+            "/home",
+    };
+    private static final String[] PUBLIC_ENDPOINTS = {
+            "/login",
+            "/users/create"
+    };
 
     @Autowired
     public SecurityConfig(PersonDetailsService personDetailsService, WebAccessDeniedHandler webAccessDeniedHandler) {
@@ -28,7 +35,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-
+                .authorizeRequests()
+                .antMatchers(ADMIN_ENDPOINTS).hasRole("ADMIN")
+                .antMatchers(PUBLIC_ENDPOINTS).permitAll()
+                // rest of urls only for authenticated users and admins
+                .anyRequest().hasAnyRole("USER", "ADMIN")
+                .and()
                 .formLogin().loginPage("/login")
                 .loginProcessingUrl("/process_login")
                 .defaultSuccessUrl("/get_home_page", true)
@@ -36,14 +48,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().logoutUrl("/logout").logoutSuccessUrl("/login");
 
-        http.exceptionHandling()
-                .accessDeniedHandler(webAccessDeniedHandler);
+
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService)
-                // spring security will automatically encode password on user login
                 .passwordEncoder(getPasswordEncoder());
     }
 
